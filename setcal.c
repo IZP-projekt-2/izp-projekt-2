a4756dfa86d926e423bd7b6217fcd8c275969fd8
Zdeněk Foltýn <zdenekfoltyn@Zdenek-MacBook-Air.local>
2021-11-23 00:13:28 +0100

    docs added

--- setcal.c
+++ setcal.c
@@ -6,53 +6,60 @@
 #define ELEMENT_MAX_SIZE 30
 #define MAX_LINES 1000
 #define UNIVERZUM_ALLOC 1024 // How many 'pointer sizes' are allocated when
-                             // expanding univerzum size. TODO: For smaller
-                             // values was getting segmentation error, maybe
-                             // look into that.
+                             // expanding univerzum size.
+                             //
+                             // Note: for smaller sizes was getting segmentation
+                             // fault when trying to realloc 5+ times. With 1024
+                             // tested up to 70k+ elements.
 
 typedef struct univerzum
 {
     unsigned len;    // Number of elements.
     unsigned size;   // How much space was allocated for elements (number of
                      // possible pointers, not size in bytes).
-    char **elements; // Dynamically allocated array of strings.
+    char **elements; // Dynamically allocated array of elements represented as
+                     // strings.
 } Univerzum;
 
 void univerzum_ctor(Univerzum *uni);
 void univerzum_dtor(Univerzum *uni);
 void univerzum_expand(Univerzum *uni);
-void univerzum_add_element(Univerzum *uni, char element[]);
+int univerzum_add_element(Univerzum *uni, char element[]);
 char *univerzum_get_element(Univerzum *uni, char element[]);
 
-/************************************************************/
-
-typedef struct relation
-{
-    char *first;
-    char *second;
-} Relation;
-
-typedef struct set
-{
-    bool contains_relations; // Elements are treated differently
-    char **elements;         // Pointers to elements in univerzum
-    unsigned len;            // Useful for printing
-} Set;
-
-void set_init(Set *set, bool realtion_set, char **elements, unsigned len);
-void set_print(Set *set);
-
-/************************************************************/
+/******************************************************************************/
 
 typedef enum
 {
-    def_univerzum = 85, // ord value of U
-    def_set = 83,       // ord value of S
-    def_relation = 82,  // ord value of R
-    exe_command = 67    // ord value of C
-} Operation;
+    uni = 85, // Ord value of U. Set containing all elements of univerzum.
+    els = 83, // Ord value of S. Set containing elements of univezum.
+    rel = 82, // Ord value of R. Set containing relations.
+    cst = 0,  // "Constant" set. TODO explain this.
+} SetType;
 
 typedef struct
+{
+    SetType type;    // Type of given set. Elements are treated differently
+                     // based on what type of set they belong to.
+    char **elements; // Pointers to elements in univerzum.
+    unsigned len;    // Number of elements.
+} Set;
+
+int set_init(Set *set, SetType type, char **elements, unsigned len);
+int set_add_element(Set *set, char *element);
+void set_print(Set *set);
+
+/******************************************************************************/
+
+typedef enum
+{
+    def_univerzum = uni,
+    def_set = els,
+    def_relation = rel,
+    exe_command = 67 // ord value of C.
+} Operation;
+
+typedef struct set
 {
     Operation operation;
     Set *related_set; // pro U - množina všech prvků
@@ -74,57 +81,88 @@ void line_ctor(Line *line);
 void line_dtor(Line *line);
 void line_link_set(Line *line, Set *set);
 
-/************************************************************/
+/******************************************************************************/
 
+/**
+ * @brief Signature of an LineParser function - function that can parse line
+ * from an input file into Line structure. Program first determines what parser
+ * to use based on the first letter of each line, then calls the parser. Parser
+ * handles reading from the file on its own, continues reading the line after
+ * first two chars (char that determines the parser and ' ' after it) untill
+ * reaching end of the line. Returns 0 when parsing was succesful, otherwise
+ * prints into stderr and returns 1.
+ */
 typedef int (*LineParser)(FILE *input, Line *target);
 
+/**
+ * @brief Links LineParser to an operation specificator.
+ */
 typedef struct operation_parser
 {
     Operation operation;
     LineParser parser;
 } OperationParser;
 
-int parse_line(unsigned index, FILE *input, Line *target);
+int parse_line(FILE *input, Line *target);
 int parse_univerzum(FILE *input, Line *target);
 int parse_set(FILE *input, Line *target);
 
-FILE *open_input_file(int argc, char **argv);
-int load_word(FILE *input, char *target, unsigned *len, unsigned maxlen);
-bool is_letter(char ch);
+/******************************************************************************/
 
 static Line lines[MAX_LINES + 1]; // Lines are 1-indexed
 static Univerzum univerzum;
 static OperationParser operation_parsers[] = {
     {def_univerzum, &parse_univerzum},
     {def_set, &parse_set},
-    {0, NULL}};
+    {0, NULL}}; // "null" element of array - used to stop iteration.
+
+/******************************************************************************/
+
+FILE *open_input_file(int argc, char **argv);
+void read_file(FILE *input_file);
+int load_word(FILE *input, char *target, unsigned *len, unsigned maxlen);
+bool is_letter(char ch);
 
 int main(int argc, char **argv)
 {
-    FILE *input_file = open_input_file(argc, argv);
     univerzum_ctor(&univerzum);
+    FILE *input_file = open_input_file(argc, argv);
+    read_file(input_file);
 
+    fclose(input_file);
+}
+
+void read_file(FILE *input_file)
+{
     for (int line_index = 1; line_index <= MAX_LINES; line_index++)
     {
         Line line;
         line_ctor(&line);
+        int parsing_res = parse_line(input_file, &line);
 
-        if (parse_line(line_index, input_file, &line))
+        if (parsing_res == EOF)
             break;
+
+        else if (parsing_res)
+        {
+            fprintf(stderr, "Preceeding error occured on line %d.\n",
+                    line_index);
+            exit(1);
+        }
 
         set_print(line.related_set);
         printf("\n");
         lines[line_index] = line;
     }
-
-    fclose(input_file);
 }
 
 /**
  * @brief Loads next word (string containing letters of english alphabet) from
  * an input file.
  *
- * @param input input stream to be continued reading
+ * TODO: maxlen doesn't work - abort trap: 6
+ *
+ * @param input stream to be continued reading
  * @param target where the loaded element terminated with '\\0'is stored
  * @param len where the lenght of loaded word is stored
  * @param maxlen maximal length of an element, not counting the '\\0' char
@@ -136,12 +174,12 @@ int load_word(FILE *input, char *target, unsigned *len, unsigned maxlen)
     char character;
     unsigned index = 0;
 
-    while (index <= maxlen)
+    for (;; index++)
     {
-        if (!is_letter(character = fgetc(input)))
+        if (!is_letter(character = fgetc(input)) || index == maxlen)
             break;
 
-        target[index++] = character;
+        target[index] = character;
     }
 
     target[index] = '\0';
@@ -189,19 +227,37 @@ FILE *open_input_file(int argc, char **argv)
     return input_file;
 }
 
-// TODO docs
-int parse_line(unsigned index, FILE *input, Line *target)
+/**
+ * @brief Reads one line from an input file and parses it to Line struct.
+ *
+ * @param input stream to be read.
+ * @param target ponter to Line struct where the result should be stored.
+ * @return 0 when parsing was succesful, EOF when reaching end of file, non-zero
+ * value when parsing fails.
+ */
+int parse_line(FILE *input, Line *target)
 {
     LineParser parser = NULL;
     char character = fgetc(input);
-    fgetc(input);
+
+    if (character == EOF)
+        return character;
+
+    if (fgetc(input) != ' ')
+    {
+        fprintf(stderr, "Expected space after first character.\n");
+        return 1;
+    }
 
     for (int i = 0; operation_parsers[i].operation != 0; i++)
         if ((Operation)character == operation_parsers[i].operation)
             parser = operation_parsers[i].parser;
 
     if (parser == NULL)
+    {
+        fprintf(stderr, "Cannot process operation '%c'.\n", character);
         return 1;
+    }
 
     return (*parser)(input, target);
 }
@@ -216,15 +272,31 @@ int parse_univerzum(FILE *input, Line *target)
         last_char = load_word(input, element, &len, ELEMENT_MAX_SIZE);
         univerzum_add_element(&univerzum, element);
 
-        if (last_char == EOF || last_char == '\n')
+        if (len == 0)
+        {
+            fprintf(stderr, "Elements separated by more than one space.\n");
+            return 1;
+        }
+
+        else if (last_char == EOF || last_char == '\n')
             break;
 
-        else if (last_char != ' ' || len == 0)
+        else if (len != ELEMENT_MAX_SIZE && last_char != ' ')
+        {
+            fprintf(stderr, "Element contains invalid characters\n");
             return 1;
+        }
+
+        else if (last_char != ' ')
+        {
+            fprintf(stderr, "Element exceeds maximal lenght (%d).\n",
+                    ELEMENT_MAX_SIZE);
+            return 1;
+        }
     }
 
     Set all_elemets;
-    set_init(&all_elemets, false, univerzum.elements, univerzum.len);
+    set_init(&all_elemets, uni, univerzum.elements, univerzum.len);
     line_link_set(target, &all_elemets);
 
     return 0;
@@ -241,15 +313,29 @@ int parse_set(FILE *input, Line *target)
         last_char = load_word(input, element, &len, ELEMENT_MAX_SIZE);
         set_elements[index] = univerzum_get_element(&univerzum, element);
 
-        if (last_char == EOF || last_char == '\n')
-            break;
-
-        else if (last_char != ' ' || len == 0 || set_elements[index++] == NULL)
+        if (len == 0)
+        {
+            fprintf(stderr, "Elements separated by more than one space.\n");
             return 1;
+        }
+
+        else if (set_elements[index++] == NULL ||
+                 (last_char != ' ' && last_char != EOF && last_char != '\n'))
+        {
+            fprintf(stderr, "Element isn't defined in univezum.\n");
+            return 1;
+        }
+
+        else if (last_char == EOF || last_char == '\n')
+            break;
     }
 
     Set set;
-    set_init(&set, false, set_elements, ++index);
+    if (set_init(&set, els, set_elements, index))
+    {
+        fprintf(stderr, "Element repeated in set definition.\n");
+        return 1;
+    }
     line_link_set(target, &set);
 
     return 0;
@@ -280,22 +366,21 @@ void univerzum_expand(Univerzum *uni)
 
     if (uni->elements == NULL)
     {
-        fprintf(stderr, "Memory allocation failed");
+        fprintf(stderr, "Memory allocation failed.\n");
         exit(1);
     }
 }
 
-// Adds string to the univerzum.
+// Adds string to the univerzum. Returns 0 of element was added succesfuly, 1 if
+// elements was already contained or the element was invalid (reserved keyword).
+//
 // TODO: Pořešit tohle:
-//   "...Prvky univerza nesmí obsahovat identifikátory příkazů a klíčová slova true a false... "
-//   "...Délka řetězce je maximálně 30 znaků..."
-//   "...Pokud se prvek v množině nebo dvojice v relaci opakuje, jedná se o chybu..." musí se
-//   kontrolovat i tady, ne jen při definici množin, v množinách se pak porovanjí jen ukazatele
-//   mezi sebou
-void univerzum_add_element(Univerzum *uni, char element[])
+//     "...Prvky univerza nesmí obsahovat identifikátory příkazů a klíčová slova
+//      true a false... "
+int univerzum_add_element(Univerzum *uni, char element[])
 {
     if (univerzum_get_element(uni, element) != NULL)
-        return; // TODO Maybe should throw an error
+        return 1;
 
     int el_size = strlen(element) + 1;
     char **element_p = &(uni->elements[(uni->len)++]);
@@ -312,6 +397,8 @@ void univerzum_add_element(Univerzum *uni, char element[])
 
     for (int i = 0; i <= el_size; i++)
         (*element_p)[i] = element[i];
+
+    return 0;
 }
 
 // Returns pointer to given element in univerzum. If no such element is
@@ -324,21 +411,37 @@ char *univerzum_get_element(Univerzum *uni, char element[])
     return NULL;
 }
 
-void set_init(Set *set, bool realtion_set, char **elements, unsigned len)
+int set_init(Set *set, SetType type, char **elements, unsigned len)
 {
-    set->contains_relations = realtion_set;
+    set->type = type;
     set->elements = malloc(sizeof(char **) * len);
-    set->len = len;
+    set->len = 0;
+
     for (int index = 0; index < len; index++)
-        set->elements[index] = elements[index];
+        if (set_add_element(set, elements[index]))
+            return 1;
+
+    return 0;
+}
+
+// Adds element to a set. Returns 0 of element was added succesfully, 1 if
+// elements was already contained.
+int set_add_element(Set *set, char *element)
+{
+    for (int i = 0; i < set->len; i++)
+        if (element == set->elements[i])
+            return 1;
+
+    set->elements[set->len++] = element;
+    return 0;
 }
 
 void set_print(Set *set)
 {
-    printf("%c ", set->contains_relations ? def_relation : def_set);
+    printf("%c ", (char)set->type);
 
     for (int i = 0; i < set->len; i++)
-        if (!set->contains_relations)
+        if (set->type == els || set->type == uni)
             printf("%s ", set->elements[i]);
 }
 
